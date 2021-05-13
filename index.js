8a2ad0ee491eb739935a73cca2258f1ceaf3b64d
nischaldutt <nischaldutt01@gmail.com>
2021-05-14 00:36:13 +0530

    changing project description

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 /*********************************
- * TAXI BOOKING MANAGEMENT SYSTEM
- * created by Nischal Dutt 
+ * CAB BOOKING MANAGEMENT SYSTEM
+ * created by Nischal Dutt
  * on 21/02/2019
-*********************************/
+ *********************************/
 
-const express                       = require('express')
-const bodyParser                    = require('body-parser')
-const swaggerUI                     = require('swagger-ui-express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const swaggerUI = require("swagger-ui-express");
 
-const db                            = require('./database/mongodb')
-const CONSTANTS                     = require('./properties/constants')
-const customerRouter                = require('./routes/customerRoutes')
-const driverRouter                  = require('./routes/driverRoutes')
-const adminRouter                   = require('./routes/adminRoutes')
-const swaggerDocument               = require('./swagger.json')
+const db = require("./database/mongodb");
+const CONSTANTS = require("./properties/constants");
+const customerRouter = require("./routes/customerRoutes");
+const driverRouter = require("./routes/driverRoutes");
+const adminRouter = require("./routes/adminRoutes");
+const swaggerDocument = require("./swagger.json");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 //ADMIN ROUTES
-app.use('/admin', adminRouter)
+app.use("/admin", adminRouter);
 
 //CUSTOMER ROUTES
-app.use('/customer', customerRouter)
+app.use("/customer", customerRouter);
 
 //DRIVER ROUTES
-app.use('/driver', driverRouter)
+app.use("/driver", driverRouter);
 
 db.connect((err) => {
-    if(err) {
-        console.log('unable to connect to mongo database')
-        process.exit(1)
-    }
-    else {
-        app.listen(process.env.PORT || 3000, () => {
-            console.log('server online !')
-        })
-    }
-})
+  if (err) {
+    console.log("unable to connect to mongo database");
+    process.exit(1);
+  } else {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("server online !");
+    });
+  }
+});
